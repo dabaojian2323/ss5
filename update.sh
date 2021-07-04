abbabd7f2d38d55f112a204f73cedca1b5b81f5e
dabaojian2323 <86605158+dabaojian2323@users.noreply.github.com>
2021-07-04 16:19:39 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# author:biu 
+read -p "请输入ss5账户: " username
+read -p "请输入ss5密码: " passwd
+echo "$username   $passwd"  >  /etc/opt/ss5/ss5.passwd
+systemctl restart ss5
+echo "修改密码成功！账号 $username  密码 $passwd"
