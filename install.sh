ac6cc92a944a54f042de1397137b02ca42ad9d55
dabaojian2323 <86605158+dabaojian2323@users.noreply.github.com>
2021-07-05 16:42:04 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#author:biu
+con1="auth 0.0.0.0/0 -  u"
+con2="permit  u  0.0.0.0/0  -   0.0.0.0/0   -    -   -  -   -"
+yum -y install pam-devel openldap-devel openssl-devel
+tar -zxvf ss5-3.8.9-8.tar.gz
+cd ss5-3.8.9
+yum -y install gcc
+./configure
+make && make install
+echo "install success and config now"
+sed -i "87c$con1"  /etc/opt/ss5/ss5.conf
+sed -i "203c$con2" /etc/opt/ss5/ss5.conf
+read -p "请输入ss5账户: " username
+read -p "请输入ss5密码: " passwd
+echo "$username   $passwd"  >  /etc/opt/ss5/ss5.passwd
+chmod 755 /etc/rc.d/init.d/ss5  &> /dev/null
+/etc/rc.d/init.d/ss5 restart     &> /dev/null
+echo "服务开启成功！账号 $username  密码 $passwd"
